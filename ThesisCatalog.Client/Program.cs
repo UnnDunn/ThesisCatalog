3ce1b88a97c8f6fa62306820b1e8d4cea0e25744
Uchendu Nwachuku <uchendu@unndunn.com>
2024-03-12 12:14:43 -0400

    Add CatalogService to client DI container

--- ThesisCatalog.Client/Program.cs
+++ ThesisCatalog.Client/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ThesisCatalog.Client;
+using ThesisCatalog.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+builder.Services.AddSingleton<CatalogService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
