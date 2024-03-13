bae46d17c7d3e74b3172ea8d8c6126f6485e6868
Uchendu Nwachuku <uchendu@unndunn.com>
2024-03-13 16:51:42 -0400

    Add ThesisCatalogJsonSerializerContext class
    This class has been added to the ThesisCatalog.Core.Entities namespace to provide a centralized location for JSON serialization. It is configured with the JsonSourceGenerationOptions attribute to use case-insensitive property name matching and multiple JsonSerializable attributes to specify types for serialization.

---
+++ ThesisCatalog.Core/Entities/JsonSerializerContext.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace ThesisCatalog.Core.Entities;
+
+[JsonSourceGenerationOptions(PropertyNameCaseInsensitive = true)]
+[JsonSerializable(typeof(ComponentDescriptor))]
+[JsonSerializable(typeof(ComponentManufacturer))]
+[JsonSerializable(typeof(ComputerCatalogItem))]
+[JsonSerializable(typeof(List<ComputerCatalogItem>))]
+[JsonSerializable(typeof(MemorySpecification))]
+[JsonSerializable(typeof(StorageSpecification))]
+[JsonSerializable(typeof(Weight))]
+[JsonSerializable(typeof(UsbSpecification))]
+[JsonSerializable(typeof(List<UsbSpecification>))]
+[JsonSerializable(typeof(Dictionary<UsbType, int>))]
+public partial class ThesisCatalogJsonSerializerContext : JsonSerializerContext
+{
+}
